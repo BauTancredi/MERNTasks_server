cc620feba5d33384e259ee53933189d342baba45
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-06-27 20:28:13 -0300

    Validating if user already exists

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,21 @@
-exports.createUser = (req, res) => {
-  console.log(req.body);
+const User = require("../models/User");
+
+exports.createUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ msg: "The user already exists" });
+
+    // Create new user
+    user = new User(req.body);
+
+    // Saves new user
+    await user.save();
+
+    res.json({ msg: "User created" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("There has been an error");
+  }
 };
