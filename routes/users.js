abcc9601898d500d5ea204b8fcd8e0c33b460034
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-06-27 21:24:21 -0300

    express-validator

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,20 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const { check } = require("express-validator");
 
 // Create user
 // api/users
-router.post("/", userController.createUser);
+router.post(
+  "/",
+  [
+    check("name", "The name is mandatory").not().isEmpty(),
+    check("email", "Add a valid email").isEmail(),
+    check("password", "Password must be at least 6 characters long").isLength({
+      min: 6,
+    }),
+  ],
+  userController.createUser
+);
 
 module.exports = router;
