9491fd37fe6314bebc0830526318c6b6094e0934
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-06-28 14:58:04 -0300

    Updating a project

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -34,3 +34,39 @@ exports.getProjects = async (req, res) => {
     res.status(500).send("There has been an error");
   }
 };
+
+// Update project
+exports.updateProject = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty())
+    return res.status(400).json({ errors: errors.array() });
+
+  const { name } = req.body;
+  const newProject = {};
+
+  if (name) newProject.name = name;
+
+  try {
+    // Check ID
+    let project = await Project.findById(req.params.id);
+
+    // Verify if project exists
+    if (!project) return res.status(404).json({ msg: "Project not found" });
+
+    // Verify the creator
+    if (project.creator.toString() !== req.user.id)
+      return res.status(401).json({ msg: "Not authorizated" });
+
+    // Update
+    project = await Project.findByIdAndUpdate(
+      { _id: req.params.id },
+      { $set: newProject },
+      { new: true }
+    );
+    res.json({ project });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server error");
+  }
+};
--- routes/projects.js
+++ routes/projects.js
@@ -6,14 +6,25 @@ const auth = require("../middleware/auth");
 
 const router = express.Router();
 
+// api/projects
+
 // Create projects
-// api/projectls
 router.post(
   "/",
   auth,
   [check("name", "The name is mandatory").not().isEmpty()],
   projectController.createProject
 );
+
+// Get all projects for a user
 router.get("/", auth, projectController.getProjects);
+
+// Update a project
+router.put(
+  "/:id",
+  auth,
+  [check("name", "The name is mandatory").not().isEmpty()],
+  projectController.updateProject
+);
 
 module.exports = router;
