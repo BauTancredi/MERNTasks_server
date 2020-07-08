347a0a0da65c6ffa2c31f7ce3a72b824cc77e09c
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-08 20:33:16 -0300

    change on port

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 // App port
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 // Import routes
 app.use("/api/users", require("./routes/users"));
@@ -25,6 +25,6 @@ app.use("/api/projects", require("./routes/projects"));
 app.use("/api/tasks", require("./routes/tasks"));
 
 // Start app
-app.listen(PORT, () => {
-  console.log(`Server is working on the port ${PORT}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is working on the port ${port}`);
 });
